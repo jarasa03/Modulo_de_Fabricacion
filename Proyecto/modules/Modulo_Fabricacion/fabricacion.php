538f30be3bb35ec86e8676028b9bbca8a1e41023
jarasa03 <javierarruabarrena03@gmail.com>
2024-11-12 12:36:52 +0100

    Ruta css actualizada

--- Proyecto/modules/Modulo_Fabricacion/fabricacion.php
+++ Proyecto/modules/Modulo_Fabricacion/fabricacion.php
@@ -44,7 +44,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fabricación</title>
-    <link rel="stylesheet" href="/css/stylesheet_m2.css">
+    <link rel="stylesheet" href="../../css/stylesheet_m2.css">
 </head>
 
 <body>
