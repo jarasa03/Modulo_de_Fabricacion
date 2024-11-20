c04ae4499ebedcaf560b4d71480ed20c19e05c1e
jarasa03 <javierarruabarrena03@gmail.com>
2024-11-20 20:59:25 +0100

    style: :art: estilos para máquinas

--- topvending/m2/maquinas.php
+++ topvending/m2/maquinas.php
@@ -118,8 +118,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Máquinas</title>
     <link rel="stylesheet" href="./css/maquinas.css">
+    <link rel="stylesheet" href="/topvending/css/hallentrada.css">
 </head>
-<header><button onclick="window.location.href='../../login.php'">Cerrar sesión</button></header>
 
 <body>
     <div id="imagen_maquina">
